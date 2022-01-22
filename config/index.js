f3f93e79faaa6a616ad8332b5ebd7e6ce8863fb6
esra <esra.sayin@perseivivareium.com>
2022-01-22 19:16:18 +0100

    third commit

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 module.exports = {
   getDbConnectionString: function () {
-    return "mongodb+srv://admin:1234@cluster0.38lv9.mongodb.net/node-todo?retryWrites=true&w=majority";
+    return "here your mongodb url";
   },
 };
